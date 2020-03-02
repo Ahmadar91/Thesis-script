39df970a8f340224dc9a69f97c478fcb5d05c7c8
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2020-03-02 22:11:27 +0100

    mysql on digitalocean

--- config.js
+++ config.js
@@ -10,10 +10,30 @@ module.exports = {
   //  clientId: 'Ahmadar91',
   // clientSecret: '60209dcd459ed9f6f954c83f0f3824cfc5ade766'
   database: {
-    host: '127.0.0.1',
+    host: '157.245.42.114',
     port: 3306,
-    user: 'root',
-    password: 'admin',
+    user: 'admin1',
+    password: 'admin1',
     database: 'ant'
   }
 }
+
+// module.exports = {
+//   repository: 'https://api.github.com/repos/Ahmadar91/ant/commits',
+//   github: {
+//     clientId: 'caesarhawi',
+//     clientSecret: '3dbf56b44c70e4641a9145943c8792f7de0315c4'
+//   },
+//   //  clientId: 'caesarhawi',
+//   //  clientSecret: '3dbf56b44c70e4641a9145943c8792f7de0315c4'
+
+//   //  clientId: 'Ahmadar91',
+//   // clientSecret: '60209dcd459ed9f6f954c83f0f3824cfc5ade766'
+//   database: {
+//     host: '127.0.0.1',
+//     port: 3306,
+//     user: 'root',
+//     password: 'admin',
+//     database: 'ant'
+//   }
+// }
